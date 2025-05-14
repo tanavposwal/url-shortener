879b72f96ab439234f2ab227078e0fd1529bb564
Tanav Poswal <tanavposwal939@gmail.com>
2025-05-14 11:08:27 +0530

    redis

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import db from "./db";
 import express from "express";
+import redis from "redis";
 
 const app = express();
 app.use(express.json());
+
+const client = redis.createClient();
+client.connect();
 
 app.get("/", (req, res) => {
   res.send("Ultra fast url shortner!");
@@ -40,6 +44,26 @@ app.post("/api/shorten", async (req, res) => {
   res.send({
     slug,
   });
+});
+
+app.get("/redirect/:slug", async (req, res) => {
+  const { slug } = req.params;
+
+  // cache layer with redis
+  if (await client.exists(`slug:${slug}`)) {
+    const long_url = client.get(`slug:${slug}`);
+    res.redirect(long_url);
+  }
+
+  const toUrl = await db.url.findFirst({
+    where: {
+      short_url: slug,
+    },
+  });
+
+  await client.set(`slug:${slug}`, toUrl?.long_url);
+
+  res.redirect(toUrl?.long_url as string);
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
